7c8161ca9df24b30a2665d0d1011fd41345d4006
Rodrigo Rocha <109789436+RodrigoRocha10@users.noreply.github.com>
2024-09-04 08:55:35 +0100

    update main

---
+++ calc_flutter/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:calc_flutter/app.dart';
+import 'package:calc_flutter/simple_bloc_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user.repository.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
+}
